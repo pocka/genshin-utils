c9bed46688417456087f8ce4f94ed3d79c2f94ce
Shota Fuji <pockawoooh@gmail.com>
2021-04-28 19:49:53 +0000

    feat(app-random-event-counter): Add little bit styles

--- packages/app-random-event-counter/src/components/App.tsx
+++ packages/app-random-event-counter/src/components/App.tsx
@@ -1,8 +1,51 @@
 import * as React from "react";
+import { RiArrowGoBackFill } from "react-icons/ri";
+import styled from "styled-components";
 
 import { CircularCounter } from "./CircularCounter";
 import { REMAINS_MAX } from "../constants";
 import { CountState } from "../states/CountState";
+
+const AppWrapper = styled.div`
+  padding: 3.2rem;
+`;
+
+const RevertButton = styled.button`
+  display: flex;
+  align-items: center;
+  position: relative;
+  margin: auto;
+  margin-block-start: 16rem;
+  padding: 0.8rem 1.6rem;
+
+  background-color: var(--theme-fg);
+  border: 0.2rem solid var(--theme-bg);
+  border-radius: 0.4rem;
+  color: var(--theme-bg);
+  overflow: hidden;
+
+  &:hover:not(:disabled) {
+    box-shadow: 0 0 0 0.15rem var(--theme-fg);
+  }
+  &:active:not(:disabled) {
+    opacity: 0.8;
+    transform: scale(0.98);
+  }
+  &:focus {
+    box-shadow: 0 0 0 0.15rem Highlight;
+    outline: none;
+  }
+  &:disabled {
+    background-color: var(--theme-fg-sub);
+    color: var(--theme-bg-sub);
+  }
+`;
+
+const UndoIcon = styled(RiArrowGoBackFill)`
+  margin-inline-end: 0.3em;
+  font-size: 0.9em;
+  vertical-align: middle;
+`;
 
 export interface AppProps {
   state: CountState;
@@ -18,16 +61,20 @@ export const App = ({ state, onChangeRemains }: AppProps) => {
       return <p>Loading</p>;
     case "loaded":
       return (
-        <div>
+        <AppWrapper>
           <CircularCounter
             count={state.data.remains}
             max={REMAINS_MAX}
             onClick={() => onChangeRemains(state.data.remains - 1)}
           />
-          <button onClick={() => onChangeRemains(state.data.remains + 1)}>
-            +1
-          </button>
-        </div>
+          <RevertButton
+            disabled={state.data.remains >= REMAINS_MAX}
+            onClick={() => onChangeRemains(state.data.remains + 1)}
+          >
+            <UndoIcon role="img" aria-label="Go back icon" />
+            <span>Undo (add 1)</span>
+          </RevertButton>
+        </AppWrapper>
       );
   }
 };
